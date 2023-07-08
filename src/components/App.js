4939c50340330f88c6da18118132fec282faccb5
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-08 13:18:42 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+        <Comp parent="Parent Component" child1="Child Component 1" child2="Child Component 2" />
     </div>
   )
+}
+
+const Comp = (props) => {
+    const [option, setOption] = useState('');
+
+    return(
+          <div className="parent">
+            <h1>{props.parent}</h1>
+            <div className="child1">
+              <h2>{props.child1}</h2>
+              <button onClick={() => setOption("Option 1")}>Option 1</button>
+            </div>
+            <div className="child2">
+              <h2>{props.child2}</h2>
+              <button onClick={() => setOption("Option 2")}>Option 2</button>
+            </div>
+            <h3>Selected Option: {option}</h3>
+          </div>
+    )
 }
 
 export default App
